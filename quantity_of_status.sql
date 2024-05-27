ef9a2dbf69fb93a43e0c4ec8d4f1b1e1460e9c7f
Amyl11 <164646202+Amyl11@users.noreply.github.com>
2024-05-27 11:26:53 +0700

    Update quantity_of_status.sql

--- quantity_of_status.sql
+++ quantity_of_status.sql
@@ -3,9 +3,9 @@ SELECT
     WHEN status = 0 THEN 'Đang được mượn'
     WHEN status = 1 THEN 'Có thể mượn'
     WHEN status = 2 THEN 'Quá hạn'
-    ELSE 'Không thể mượn'
-  END AS "Mã trạng thái",
-  status AS "Giá trị trạng thái",
+    WHEN status = -1 THEN 'Không thể mượn'
+  END AS "Trạng thái",
+  status AS "Mã trạng thái",
   COUNT(*) AS "Số lượng"
 FROM books
 GROUP BY status
