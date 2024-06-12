354fc1a5545ff9cbe0e2091cf133ec773ac61710
Amyl11 <164646202+Amyl11@users.noreply.github.com>
2024-06-12 08:00:01 +0700

    Add files via upload

---
+++ re.sql
@@ -0,0 +1,57 @@
+create table re (
+	reserve_date DATE,
+	return_date DATE,
+	due DATE,
+	user_id INT,
+	book_id INT
+);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-07-15 17:47:14', '2024-05-28 12:16:13', '6/28/2023', 10, 139);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-02-25 17:51:25', '2024-01-16 04:57:13', '7/27/2023', 18, 164);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-12-07 18:31:11', '2024-05-13 01:41:35', '5/9/2024', 13, 34);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-02 01:50:45', '2023-10-26 12:25:19', '11/30/2023', 14, 17);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-10-04 12:49:51', '2023-12-23 23:13:20', '3/23/2024', 6, 105);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-13 07:11:04', '2023-10-17 02:51:01', '9/5/2023', 16, 65);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-03-24 22:54:09', '2023-09-06 02:36:24', '7/28/2023', 11, 227);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-04 17:24:44', '2024-04-21 05:38:41', '7/23/2023', 8, 166);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-11 21:39:54', '2023-11-18 11:46:34', '9/15/2023', 17, 116);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-05-11 09:37:46', '2023-07-12 07:33:30', '9/8/2023', 12, 55);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-09-04 00:25:12', '2023-08-02 13:56:36', '3/13/2024', 8, 9);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-05-03 12:24:22', '2024-04-12 07:32:35', '1/29/2024', 14, 13);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-07-15 03:04:49', '2024-02-02 12:44:49', '8/6/2023', 15, 209);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-06-23 00:43:03', '2024-02-08 18:46:23', '10/23/2023', 18, 138);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-03-04 08:21:25', '2024-02-20 12:14:06', '6/25/2023', 18, 81);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-14 12:33:27', '2024-01-03 12:44:10', '7/31/2023', 11, 182);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-12-21 11:44:05', '2023-10-27 15:29:55', '5/30/2024', 13, 142);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-07-23 19:08:52', '2023-08-12 06:33:29', '3/30/2024', 6, 115);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-04-17 00:00:52', '2024-04-11 10:49:11', '9/24/2023', 3, 165);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-06-19 15:42:40', '2023-06-13 23:15:58', '7/18/2023', 16, 2);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-07-27 03:09:37', '2024-03-31 21:35:56', '5/16/2024', 7, 13);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-01-04 18:17:59', '2023-09-16 23:13:05', '2/27/2024', 16, 1);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-12-27 06:25:40', '2023-10-29 14:01:21', '2/19/2024', 18, 21);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-01-26 10:56:58', '2023-10-27 10:29:38', '3/21/2024', 12, 33);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-02-19 01:17:27', '2024-06-06 00:11:12', '10/1/2023', 19, 52);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-12-09 00:06:10', '2023-12-16 02:23:14', '8/21/2023', 10, 206);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-01-19 02:45:27', '2023-09-14 03:28:39', '1/14/2024', 20, 112);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-07-13 04:50:50', '2023-06-15 12:53:50', '4/12/2024', 17, 42);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-10-18 22:04:49', '2024-03-02 02:04:15', '4/20/2024', 14, 187);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-08-20 11:55:34', '2023-08-03 16:23:20', '2/10/2024', 4, 104);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-06-24 16:23:09', '2023-07-28 15:49:13', '11/26/2023', 18, 63);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-05-10 21:50:56', '2024-02-02 01:15:34', '7/8/2023', 13, 149);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-09-10 12:33:50', '2023-11-21 16:15:33', '12/16/2023', 3, 251);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-06-30 10:13:09', '2023-09-23 23:51:28', '3/29/2024', 6, 78);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-05-14 07:18:35', '2023-07-10 00:24:51', '5/24/2024', 2, 2);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-07-07 06:09:58', '2023-07-14 14:28:15', '6/19/2023', 14, 229);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-27 05:13:55', '2024-02-12 15:34:54', '5/4/2024', 11, 265);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-04-24 16:28:06', '2024-05-28 14:50:40', '11/5/2023', 19, 72);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-05-11 18:28:10', '2023-12-14 13:50:35', '10/27/2023', 14, 291);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-07-01 22:13:46', '2023-07-13 04:52:48', '10/10/2023', 5, 199);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-09-02 19:12:07', '2024-05-15 02:16:02', '5/7/2024', 17, 192);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-21 03:31:26', '2023-07-15 19:43:29', '10/24/2023', 12, 63);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-13 18:22:01', '2024-05-26 08:50:58', '9/27/2023', 2, 111);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-07-30 10:05:08', '2024-04-05 20:30:40', '9/23/2023', 20, 68);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-09 22:28:41', '2023-12-05 17:22:48', '7/30/2023', 18, 174);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-11-27 13:58:19', '2024-04-25 04:03:13', '10/4/2023', 15, 176);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-04-24 10:19:46', '2023-09-26 20:40:29', '10/3/2023', 20, 71);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-09-05 18:54:59', '2024-01-19 00:28:56', '5/16/2024', 2, 281);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2024-01-18 03:32:26', '2024-05-13 13:23:55', '10/10/2023', 2, 167);
+insert into re (reserve_date, return_date, due, user_id, book_id) values ('2023-07-10 21:26:25', '2023-10-24 08:33:52', '12/13/2023', 12, 40);
---
+++ report.sql
@@ -0,0 +1,58 @@
+create table report (
+	report_id INT,
+	status INT,
+	available VARCHAR(50),
+	reserved_book VARCHAR(50),
+	user_id INT,
+	book_id INT
+);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (1, -1, null, 'dmattek0@acquirethisname.com', 20, 268);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (2, 1, null, 'sgranham1@stumbleupon.com', 18, 267);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (3, 0, null, 'cbamblett2@cmu.edu', 17, 132);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (4, 2, null, 'nhalsall3@yellowbook.com', 11, 42);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (5, 1, null, 'adulake4@mail.ru', 7, 169);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (6, 1, null, 'fheugel5@ycombinator.com', 6, 177);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (7, -1, null, 'mblencoe6@netscape.com', 5, 114);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (8, 2, null, 'amalafe7@oracle.com', 8, 172);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (9, 2, null, 'cwintle8@archive.org', 10, 18);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (10, 2, null, 'mklima9@dot.gov', 18, 17);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (11, 2, null, 'btoda@github.com', 6, 160);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (12, 2, null, 'osirrellb@barnesandnoble.com', 7, 157);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (13, -1, null, 'aclaussonc@scribd.com', 10, 237);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (14, 0, null, 'pwordsworthd@dmoz.org', 3, 242);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (15, 0, null, 'estarbucke@godaddy.com', 16, 108);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (16, 2, null, 'sgunnerf@facebook.com', 17, 148);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (17, 2, null, 'oconlong@networkadvertising.org', 18, 78);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (18, 0, null, 'fbarwish@multiply.com', 8, 181);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (19, 1, null, 'adecourceyi@facebook.com', 18, 84);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (20, 2, null, 'jtranterj@sciencedirect.com', 4, 110);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (21, 2, null, 'geslemontk@usatoday.com', 7, 173);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (22, 1, null, 'wrizonl@walmart.com', 7, 242);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (23, -1, null, 'jmccreathm@psu.edu', 9, 278);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (24, 2, null, 'cfawcknern@ow.ly', 19, 282);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (25, 0, null, 'bcrasso@biglobe.ne.jp', 9, 66);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (26, -1, null, 'lmallingp@elegantthemes.com', 14, 245);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (27, 0, null, 'bfinleyq@un.org', 9, 39);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (28, 1, null, 'bdombrellr@clickbank.net', 2, 86);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (29, -1, null, 'rmaccallions@symantec.com', 6, 116);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (30, 1, null, 'nfeldmannt@comcast.net', 9, 15);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (31, -1, null, 'ssandalu@theglobeandmail.com', 8, 200);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (32, 2, null, 'bwaddilovev@google.fr', 5, 132);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (33, 2, null, 'asobeyw@senate.gov', 11, 16);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (34, 0, null, 'jclohessyx@sciencedaily.com', 2, 9);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (35, -1, null, 'rhoundsony@sfgate.com', 7, 213);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (36, 0, null, 'sroccoz@macromedia.com', 11, 252);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (37, 0, null, 'ehynson10@oracle.com', 18, 11);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (38, 1, null, 'aarnefield11@woothemes.com', 13, 134);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (39, 1, null, 'plamas12@themeforest.net', 18, 295);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (40, 0, null, 'ashuttle13@zdnet.com', 14, 73);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (41, 0, null, 'rgodson14@theatlantic.com', 16, 200);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (42, 0, null, 'nstemp15@miitbeian.gov.cn', 5, 195);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (43, -1, null, 'jhanburry16@xrea.com', 14, 42);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (44, 2, null, 'hfairham17@shareasale.com', 2, 233);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (45, 0, null, 'pbarniss18@salon.com', 11, 210);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (46, 2, null, 'fpadginton19@rediff.com', 7, 113);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (47, 2, null, 'cgylle1a@topsy.com', 11, 151);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (48, 2, null, 'dlamacraft1b@ifeng.com', 5, 289);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (49, 2, null, 'dgiorgetti1c@angelfire.com', 19, 284);
+insert into report (report_id, status, available, reserved_book, user_id, book_id) values (50, 2, null, 'wminocchi1d@parallels.com', 5, 275);
